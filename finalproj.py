9094d19c5559941e73584d97f1b45b8aa6111f2d
mohitt13 <49208187+mohitt13@users.noreply.github.com>
2020-05-03 01:45:03 +0530

    userList , admin login, User Db
    summary , extra additions, frontend improvement , {Left yet}

--- finalproj.py
+++ finalproj.py
@@ -14,46 +14,61 @@ root = os.getcwd()
 
 
 file = "m"
+client = pymongo.MongoClient(
+        "mongodb+srv://mohit13:vitsucks13@m13-kpbkz.mongodb.net/test?retryWrites=true&w=majority")
+db = client.trail
+coll = db.test
 
-
-@app.route('/')
+@app.route('/' , methods=['GET', 'POST'])
 def first_page():
     return render_template('first.html')
+    
 
 
 @app.route('/redirect', methods=['GET', 'POST'])
 def redirect():
+    if request.form['action']=='User':
+        return render_template('Frontend.html')
+    if request.form['action']=='Admin':
+        return render_template('adminfp.html')
+
+
+@app.route('/newUser',methods=['GET','POST'])
+def NewUser():
+    if request.form['submit']=='signup':
+        name = request.form['name']
+        password = request.form['pass']
+        aadhar = request.form['aadhar']
+        client = pymongo.MongoClient(
+        "mongodb+srv://mohit13:vitsucks13@m13-kpbkz.mongodb.net/test?retryWrites=true&w=majority")
+        db = client.trail
+        user = db.user
+        post = {
+            "_id":aadhar,
+            "name":name,
+            "pass":password
+        }
+        user.insert_one(post)
+    elif request.form['submit']=='login':
+        return render_template('Frontend.html')
     return render_template('Frontend.html')
-
-
-@app.route('/re', methods=['GET', 'POST'])
-def re():
-    return render_template('adminfp.html')
 
 
 @app.route('/send', methods=['GET', 'POST'])
 def send():
-
-    if request.method == 'POST':
+    if request.form['Login']=='login':
+        global file
         file = request.form['id']
-        path = root
-        path = path+"\\" + file
-        if os.path.isdir(path):
-            os.chdir(path)
-        else:
-            os.mkdir(path)
-            os.chdir(path)
-
         return render_template('upload_page.html')
+    elif request.form['Login']=='signup':
+        return render_template('registration.html')
+
     return render_template('Frontend.html')
 
 
 @app.route('/pdf_upload', methods=['GET', 'POST'])
 def pdf_upload():
-    client = pymongo.MongoClient(
-        "mongodb+srv://mohit13:vitsucks13@m13-kpbkz.mongodb.net/test?retryWrites=true&w=majority")
-    db = client.trail
-    coll = db.test
+    global coll
     doc =[]
     if request.method == 'POST':
         files = request.files.getlist('file')
@@ -71,20 +86,11 @@ def pdf_upload():
                 text += pageObj.extractText()
 
             name = name[:-4]
-            
+            # uploadfile = open("new.txt","w")
+            # uploadfile.write(text)
+            # uploadfile.close()
             doc.append(text)
-            # ON Device.................................................
-            #     if os.path.isfile(name):
-            #         f = open(name + ".txt", "a")
-            #     else:
-            #         f = open(name + ".txt", "w+")
-
-            #     f.write(text)
-            #     f.close()
-
-            # path = root
-            # return render_template('uploadedpdf.html', file=text)
-            # os.rmdir(path)
+            
         def insertdoc(pt):
             data ={
                 "_id":file,
@@ -94,6 +100,24 @@ def pdf_upload():
         insertdoc(doc)
     return render_template('upload_page.html')
 
+
+@app.route('/AdminLogin', methods=['GET','POST'])
+def admin_login():
+    if request.method=='POST':
+        AdminId = request.form['Aid']
+    return render_template('adminSp.html')
+
+@app.route('/verify', methods=['GET','POST'])
+def verify_user():
+    collection = db.user
+    if request.form['ud']=='User List':
+        data =[]
+        for x in collection.find({},{ "_id": 1 , "name":1}):
+            data.append(x)
+
+        return render_template('onclick_list.html',data=data)
+    elif request.form['ud']=='Verify Documents': 
+        return render_template('Fetch.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
